d741afae21992de39b4fbbd1fde2057a915b184a
JayBadass <115525148+JayBadass@users.noreply.github.com>
2023-09-07 20:41:19 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var result = 0
+    if n <= 7 {
+        result = 1
+    } else {
+        if n % 7 == 0 {
+            result = n / 7
+        } else {
+            result = n / 7 + 1
+        }
+    }
+    
+    return result
+}
