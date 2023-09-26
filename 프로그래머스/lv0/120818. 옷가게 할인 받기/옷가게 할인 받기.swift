2a2719a8352afc2aaa1f862479a9366a2be5dace
JayBadass <115525148+JayBadass@users.noreply.github.com>
2023-09-26 22:25:31 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ price:Int) -> Int {
+    var discountRate: Double = 0
+    
+    if price >= 500000 {
+        discountRate = 0.20
+    } else if price >= 300000 {
+        discountRate = 0.10
+    } else if price >= 100000 {
+        discountRate = 0.05
+    }
+    
+    let discountedPrice = Double(price) * (1 - discountRate)
+    return Int(discountedPrice)
+}
