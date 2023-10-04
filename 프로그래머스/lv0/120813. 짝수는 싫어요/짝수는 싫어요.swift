70023d94f69f7d0531840be3442dfaa95036e67d
JayBadass <115525148+JayBadass@users.noreply.github.com>
2023-10-04 13:34:13 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ n:Int) -> [Int] {
+    var result = [Int]()
+    for i in 0...n {
+        if i % 2 != 0 {
+            result.append(i)
+        }
+    }
+    return result
+}
