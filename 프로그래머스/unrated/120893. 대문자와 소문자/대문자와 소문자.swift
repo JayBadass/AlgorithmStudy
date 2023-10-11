0576ef6eee73480358ab0cee0165c40243ca7d60
JayBadass <115525148+JayBadass@users.noreply.github.com>
2023-10-11 21:35:04 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.26 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ my_string:String) -> String {
+    var result = ""
+
+    for char in my_string {
+        if char.isLowercase { 
+            result += String(char.uppercased())
+        } else {  
+            result += String(char.lowercased())
+        }
+    }
+
+    return result
+}
